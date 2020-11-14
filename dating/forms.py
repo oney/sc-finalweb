b8799fd639a8d8c53720e2b121f8112bf1f83005
Wan-Huang Yang <wyang47@jhu.edu>
2020-11-13 22:26:37 -0500

    register

--- dating/forms.py
+++ dating/forms.py
@@ -1,6 +1,43 @@
 from django import forms
 
 
-class UserForm(forms.Form):
-    email = forms.CharField(label="Email", max_length=255)
-    password = forms.CharField(label="Password", max_length=255, widget=forms.PasswordInput)
+class LoginForm(forms.Form):
+    email = forms.CharField(
+        label="Email",
+        max_length=255,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+        )
+    password = forms.CharField(
+        label="Password",
+        max_length=255,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        )
+
+
+class RegisterForm(forms.Form):
+    genders = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+    )
+
+    email = forms.CharField(
+        label="Email",
+        max_length=255,
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label="Password",
+        max_length=200,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    confirm_password = forms.CharField(
+        label="Confirm Password",
+        max_length=200,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    name = forms.CharField(
+        label="Name",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    gender = forms.ChoiceField(label='Gender', choices=genders)
--- dating/views.py
+++ dating/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.hashers import check_password, make_password
+from . import models
+from .forms import LoginForm, RegisterForm
 
 def index(request):
     template = 'index.html'
@@ -8,29 +10,62 @@ def index(request):
     return render(request, template, context)
 
 def login(request):
+    if request.session.get('is_login', None):
+        return redirect('/index')
+
     if request.method == "POST":
-        email = request.POST.get('email', None)
-        password = request.POST.get('password', None)
-        message = "Some fields are empty"
-        if email and password:
-            email = email.strip()
+        form = LoginForm(request.POST)
+        message = "Some fields are invalid"
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
             try:
                 user = models.User.objects.get(email=email)
-                if user.password == password:
+                if check_password(password, user.password):
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.email
                     return redirect('/index/')
                 else:
                     message = "Password is wrong"
             except:
                 message = "User not found"
+        return render(request, 'dating/login.html', locals())
 
-        return render(request, 'dating/login.html', {"message": message})
-
-    return render(request,'dating/login.html')
+    form = LoginForm()
+    return render(request, 'dating/login.html', locals())
 
 def register(request):
-    pass
-    return render(request,'dating/register.html')
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        message = "Some fields are invalid"
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            confirm_password = form.cleaned_data['confirm_password']
+            gender = form.cleaned_data['gender']
+            if password != confirm_password:
+                message = "Confirm password not match"
+                return render(request, 'dating/register.html', locals())
+            else:
+                user = models.User.objects.filter(email=email)
+                if user:
+                    message = 'Email is already registered'
+                    return render(request, 'dating/register.html', locals())
+
+                user = models.User.objects.create()
+                user.name = name
+                user.password = make_password(password)
+                user.email = email
+                user.gender = gender
+                user.save()
+                return redirect('/login/')  # 自动跳转到登录页面
+    form = RegisterForm()
+    return render(request, 'dating/register.html', locals())
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        return redirect("/index/")
+    request.session.flush()
     return redirect('/index/')
