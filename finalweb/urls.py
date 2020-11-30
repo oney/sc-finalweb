d2ee429f6b8ca3ccac6c873f88064c2f4975c6af
Wan-Huang Yang <wyang47@jhu.edu>
2020-11-29 22:49:45 -0500

    fix static files

--- finalweb/urls.py
+++ finalweb/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from dating import views
 
 
@@ -37,5 +38,13 @@ urlpatterns = [
     path('chat/<int:id>/', views.chat, name='chat'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns.append(url(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT
+    }))
+    urlpatterns.append(url(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATICFILES_DIRS[0]
+    }))
