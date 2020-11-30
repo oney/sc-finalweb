8a94dc3ddb1f1690289f2d89a4bfcbabd9f06a15
Wan-Huang Yang <wyang47@jhu.edu>
2020-11-29 20:56:50 -0500

    doc detection

--- dating/detection/detect.py
+++ dating/detection/detect.py
@@ -5,9 +5,26 @@ from .model import predict_one
 
 
 def detect(src):
+    '''
+    Detect an image
+
+    **Parameters**
+
+        src: *str*
+            The file path of the image
+
+    **Returns**
+
+        detection: *bool*
+            Return True if detecting violation
+
+    '''
     with open(src, 'r+b') as f:
         with Image.open(f) as image:
+            # Central scaling crop the image to 128x128
             cover = resizeimage.resize_cover(image, [128, 128])
+            # Covert PIL image to RGB numpy array (3 color channels)
             pix = np.array(cover.convert("RGB"))
             pred, _ = predict_one(pix)
+            # Prediction 0 means detection
             return pred == 0
--- dating/detection/model.py
+++ dating/detection/model.py
@@ -19,12 +19,31 @@ torch.backends.cudnn.deterministic = True
 
 
 class AlexNet(nn.Module):
+    '''
+    PyTorch neural network model
+    '''
     def __init__(self, output_dim):
+        '''
+        The class initializer
+
+        **Parameters**
+
+            output_dim: *int*
+                Dimension of output (classes of targets)
+
+        '''
         super().__init__()
 
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, 12, 4, 1),
             # in_channels, out_channels, kernel_size, stride, padding
+            # in_channels: color channels
+            # out_channels: the number of filters
+            # kernel_size: scanning kernel size
+            # stride: stride when scanning
+            # padding: added padding
+            # output 2D size calculation: https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html#torch.nn.Conv2d
+
             nn.MaxPool2d(2),  # kernel_size
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 192, 3, padding=1),
@@ -50,6 +69,23 @@ class AlexNet(nn.Module):
         )
 
     def forward(self, x):
+        '''
+        Forward computation of the CNN
+
+        **Parameters**
+
+            x: *<torch.Tensor[batch size, color channels, width, height]>*
+                A batch of tensor inputs
+
+        **Returns**
+
+            x: *<torch.Tensor[batch size, output dim]>*
+                A batch of tensor prediction
+
+            h: *<torch.Tensor[batch size, output dim]>*
+                A batch of tensor Conv2d output
+
+        '''
         x = self.features(x)
         h = x.view(x.shape[0], -1)
         x = self.classifier(h)
@@ -60,43 +96,113 @@ model = AlexNet(2)
 device = torch.device('cpu')
 model = model.to(device)
 dir_path = os.path.dirname(os.path.realpath(__file__))
+# Load trained model
 model.load_state_dict(torch.load(
     os.path.join(dir_path, 'best.pt'),
     map_location=device))
 
 
 class EvaluateDataset(Dataset):
+    '''
+    Dataset for evaluation
+    '''
     def __init__(self, image):
+        '''
+        The class initializer
+
+        **Parameters**
+
+            image: *numpy.ndarray[width, height, color channels]*
+                Numpy array of an image
+
+        '''
         self.image = image
         self.transform = transforms.Compose([
             transforms.ToTensor(),
             ])
 
     def __len__(self):
+        '''
+        Return size of the dataset
+
+        **Returns**
+            len: size of the dataset
+
+        '''
         return 1
 
     def __getitem__(self, idx):
+        '''
+        Return an item (image tensor and label) of dataset by idx
+
+        **Parameters**
+
+            idx: *int*
+                The index of getting item
+
+        **Returns**
+
+            item: *<list, (torch.Tensor[color channels, width, height], int)>*
+                Image tensor and label (0 or 1) in a list
+
+        '''
         image = self.transform(self.image)
         return [image, 1]
 
 
 def predict(iterator):
+    '''
+    Predict a batch of images
+
+    **Parameters**
+
+        iterator: *torch.utils.data.dataloader.DataLoader*
+            A batch of images
+
+    **Returns**
+
+        probability: *<torch.Tensor[batch size, float]>*
+            A batch of probability of each prediction
+
+        prediction: *<torch.Tensor[batch size, int]>*
+            A batch of prediction (0 or 1)
+
+    '''
     model.eval()
+    # No need to track gradients in evaluation
     with torch.no_grad():
-        for (x, y) in iterator:
+        for (x, _) in iterator:
             x = x.to(device)
-
             y_pred, _ = model(x)
-
             y_prob = F.softmax(y_pred, dim=-1)
+            # Get the label of the highest probability
             top_pred = y_prob.argmax(1, keepdim=True)
 
             return y_prob, top_pred
 
 
 def predict_one(image):
+    '''
+    Predict an image
+
+    **Parameters**
+
+        image: *numpy.ndarray[width, height, color channels]*
+            Numpy array of an image
+
+    **Returns**
+
+        prediction: *int*
+            Prediction (0 or 1)
+
+        probability: *<numpy.ndarray[float]>*
+            Probability of each prediction
+
+    '''
     dataset = EvaluateDataset(image)
+    # Generate subset dataset from dataset for prediction
     split_data, _ = data.random_split(dataset, [1, 0])
     iterator = data.DataLoader(split_data, batch_size=1)
     prob, pred = predict(iterator)
+    # Extract the prediction and probability from the batch
     return pred[0][0].item(), prob[0].numpy()
