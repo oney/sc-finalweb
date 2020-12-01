f4e8d09939f93502300d3911998167902ff464f4
Wan-Huang Yang <wyang47@jhu.edu>
2020-12-01 16:07:35 -0500

    add jwt test

--- dating/tests.py
+++ dating/tests.py
@@ -1,3 +1,28 @@
+import time
+import jwt
 from django.test import TestCase
+from .helpers import jwt_encode, jwt_decode
 
-# Create your tests here.
+
+class JWTTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_jwt(self):
+        token = jwt_encode({
+            "payload": 1234,
+            "exp": int(time.time() + 60*60*24*60)
+            })
+        self.assertTrue(isinstance(token, str))
+        info = jwt_decode(token)
+        self.assertTrue(isinstance(info, dict))
+        self.assertEqual(info['payload'], 1234)
+
+    def test_expire(self):
+        token = jwt_encode({
+            "payload": 1234,
+            "exp": int(time.time() - 1)
+            })
+
+        with self.assertRaises(jwt.exceptions.ExpiredSignatureError):
+            _ = jwt_decode(token)
